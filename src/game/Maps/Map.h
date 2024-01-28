c4a2296f867209175511fdc9d563288600633f4e
Foereaper <foereaper@elunatech.com>
2024-01-28 03:46:46 +0100

    Add Eluna multistate support
    Missing async query support, needs to be researched
    
    Currently untested other than it compiles

--- src/game/Maps/Map.h
+++ src/game/Maps/Map.h
@@ -40,6 +40,9 @@
 #include "Maps/MapDataContainer.h"
 #include "Util/UniqueTrackablePtr.h"
 #include "World/WorldStateVariableManager.h"
+#ifdef BUILD_ELUNA
+#include "LuaEngine/LuaValue.h"
+#endif
 
 #include <bitset>
 #include <functional>
@@ -47,6 +50,9 @@
 
 struct CreatureInfo;
 class Creature;
+#ifdef BUILD_ELUNA
+class Eluna;
+#endif
 class Unit;
 class WorldPacket;
 class InstanceData;
@@ -434,6 +440,12 @@ class Map : public GridRefManager<NGridType>
         bool HasActiveZone(uint32 zoneId) { return find(m_activeZones.begin(), m_activeZones.end(), zoneId) != m_activeZones.end(); }
 #endif
 
+#ifdef BUILD_ELUNA
+        Eluna* GetEluna() const;
+
+        LuaVal lua_data = LuaVal({});
+#endif
+
 #ifdef BUILD_SOLOCRAFT
         bool SoloCraftDebuffEnable = 1;
         float SoloCraftSpellMult = 1.0;
@@ -595,6 +607,10 @@ class Map : public GridRefManager<NGridType>
         ZoneDynamicInfoMap m_zoneDynamicInfo;
         ZoneDynamicInfoMap m_areaDynamicInfo;
         uint32 m_defaultLight;
+
+#ifdef BUILD_ELUNA
+        Eluna* eluna;
+#endif
 };
 
 class WorldMap : public Map
