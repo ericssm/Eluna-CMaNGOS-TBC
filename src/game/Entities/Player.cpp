acf19f6a66e8f85104d9f8237bb4551ab0a3bf20
Niam5 <ebakkedahl@gmail.com>
2024-12-20 10:14:54 -0800

    Fix infinite loop for OnDuelEnd hook

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -7331,11 +7331,7 @@ void Player::DuelComplete(DuelCompleteType type)
         data << GetName();
         SendMessageToSet(data, true);
     }
-#ifdef BUILD_ELUNA
-    // used by eluna
-    if (Eluna* e = GetEluna())
-        e->OnDuelEnd(duel->opponent, this, type);
-#endif
+
     // Remove Duel Flag object
     if (GameObject* obj = GetMap()->GetGameObject(GetGuidValue(PLAYER_DUEL_ARBITER)))
         duel->initiator->RemoveGameObject(obj, true);
@@ -7381,10 +7377,20 @@ void Player::DuelComplete(DuelCompleteType type)
     duel->opponent->SetGuidValue(PLAYER_DUEL_ARBITER, ObjectGuid());
     duel->opponent->SetUInt32Value(PLAYER_DUEL_TEAM, 0);
 
+#ifdef BUILD_ELUNA
+    Player* duelOpponent = duel->opponent;
+#endif
+
     delete duel->opponent->duel;
     duel->opponent->duel = nullptr;
     delete duel;
     duel = nullptr;
+
+#ifdef BUILD_ELUNA
+    // used by eluna
+    if (Eluna* e = GetEluna())
+        e->OnDuelEnd(duelOpponent, this, type);
+#endif
 }
 
 //---------------------------------------------------------//
