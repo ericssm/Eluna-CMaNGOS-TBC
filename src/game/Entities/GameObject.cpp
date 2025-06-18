84147f0c8310473235ebd776f2c1b88cbdb82455
ericssm <ericsha@sina.com>
2025-06-18 21:05:04 +0800

    merge with new version
    Signed-off-by: ericssm <ericsha@sina.com>

--- src/game/Entities/GameObject.cpp
+++ src/game/Entities/GameObject.cpp
@@ -1995,7 +1995,9 @@ void GameObject::Use(Unit* user, SpellEntry const* spellInfo)
     SpellCastTargets targets;
     targets.setUnitTarget(user);
 
-    spell->SpellStart(&targets);
+    SpellCastResult result = spell->SpellStart(&targets);
+    if (result == SPELL_CAST_OK)
+        onSuccess();
 }
 
 // overwrite WorldObject function for proper name localization
