0b3b489d1b33b81e08fbb98383a0da74e5c1e66b
Niam5 <ebakkedahl@gmail.com>
2025-12-06 19:11:33 -0800

    Mod: SPP Solocraft
    -Adds SoloCraft as an option to CMake to enable/disable mod
    -Updated CI files to test on push and build one daily release with SoloCraft
    
    Credit to acidmanifesto and the TrinityCore project for the heavy legwork on getting this created

--- src/game/Entities/CharacterHandler.cpp
+++ src/game/Entities/CharacterHandler.cpp
@@ -1016,6 +1016,19 @@ void WorldSession::HandlePlayerLogin(LoginQueryHolder* holder)
         e->OnLogin(pCurrChar);
 #endif
 
+#ifdef BUILD_SOLOCRAFT
+    bool SoloCraftEnable = sWorld.getConfig(CONFIG_BOOL_SOLOCRAFT_ENABLED);
+    bool SoloCraftAnnounceModule = sWorld.getConfig(CONFIG_BOOL_SOLOCRAFT_ANNOUNCE);
+
+    if (SoloCraftEnable)
+    {
+        if (SoloCraftAnnounceModule)
+        {
+            ChatHandler(pCurrChar->GetSession()).SendSysMessage("This server is running |cff4CFF00SPP SoloCraft Custom |rmodule.");
+        }
+    }
+#endif
+
     delete holder;
 }
 
